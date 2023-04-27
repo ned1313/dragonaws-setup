1860a1e967ff2716fbe65a5b540843a28b7b4736
ned1313 <ned.bellavance@gmail.com>
2023-04-27 17:25:58 -0400

    add s3 bucket

--- main.tf
+++ main.tf
@@ -11,3 +11,32 @@ module "dragondrop-compute" {
   service_account_name      = "${var.naming_prefix}-dragon-drop-service-account"
 }
 
+# Create an S3 bucket for migration history
+resource "aws_s3_bucket" "migration_history" {
+  bucket = "${var.naming_prefix}-migration-history"
+  acl    = "private"
+}
+
+# Grant service account access to S3 bucket
+resource "aws_iam_role_policy" "main" {
+    name = "${var.naming_prefix}-migration-history"
+    role = "dragondrop-fargate-runner"
+
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.migration_history.bucket}",
+                "arn:aws:s3:::${aws_s3_bucket.migration_history.bucket}/*"
+            ]
+    }
+  ]
+}
+EOF
+}
